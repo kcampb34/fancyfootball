e26805eecaf17b3a423d07d18a2ef42dabd11d30
Braeden-moe <bmorri16@student.fitchburgstate.edu>
2025-03-25 21:24:41 -0400

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,48 +1,55 @@
 <html>
-  <head>
+<head>
     <meta charset="UTF-8">
-    <title></title>
-  <br>
-  <?php include 'header.php' ?>
+    <title>Sign Up</title>
+    <br>
+    <?php include 'header.php'; ?>
+    <style>
+        .error { color: red; font-weight: bold; }
+        .success { color: green; font-weight: bold; }
+    </style>
 </head>
 <body>
-  <div class="w3-container">
-    <h1>Please complete the Sign Up Sheet below to register</h1>
-    <form name="register" action="registerAction.php" method="get">
-      <div class="item">
-        <label>Username</label>
-        <input type="text" name="user" size="40" required/>
-      </div>
-        <div class="item">
-        <label>First name</label>
-        <input type="text" name="fname" size="40" required/>
-      </div>
-        <div class="item">
-        <label>Last name</label>
-        <input type="text" name="lname" size="40" required/>
-      </div>
-      <div class="item">
-        <label for="pswd">Password</label>
-        <input type="password" name="pswd" size="40" required/>
-      </div>
-      <div class="item">
-        <label for="pswd">Retype Password</label>
-        <input type="password" name="pswd2" size="40" required/> 
-      </div>
-      <div class="item">
-          <label for="email">EMail</label>
-        <input type="email" name="email" size="40" required/>
-      </div>
-        <div class="item">
-        <label>Security Question: Favorite NFL Team?</label>
-        <input type="text" name="fteam" size="40" required/>
-      </div>
-      <div class="item">
-        <input type="submit" value="Submit" />
-        <input type="reset" value="Reset" />
-      </div>
-    </form>
-  </div>
-  <br>
+    <div class="w3-container">
+        <h1>Please complete the Sign Up Sheet below to register</h1>
+
+        <form name="register" action="registerAction.php" method="get">
+            <div class="item">
+                <label>Username</label>
+                <?php if (isset($_GET['error_user'])) echo "<p class='error'>" . htmlspecialchars($_GET['error_user']) . "</p>"; ?>
+                <input type="text" name="user" size="40" required />
+            </div>
+            <div class="item">
+                <label>First name</label>
+                <input type="text" name="fname" size="40" required />
+            </div>
+            <div class="item">
+                <label>Last name</label>
+                <input type="text" name="lname" size="40" required />
+            </div>
+            <div class="item">
+                <label for="pswd">Password</label>
+                <?php if (isset($_GET['error_password'])) echo "<p class='error'>" . htmlspecialchars($_GET['error_password']) . "</p>"; ?>
+                <input type="password" name="pswd" size="40" required />
+            </div>
+            <div class="item">
+                <label for="pswd">Retype Password</label>
+                <input type="password" name="pswd2" size="40" required />
+            </div>
+            <div class="item">
+                <label for="email">Email</label>
+                <?php if (isset($_GET['error_email'])) echo "<p class='error'>" . htmlspecialchars($_GET['error_email']) . "</p>"; ?>
+                <input type="email" name="email" size="40" required />
+            </div>
+            <div class="item">
+                <input type="submit" value="Submit" />
+                <input type="reset" value="Reset" />
+            </div>
+        </form>
+
+        <!-- Display global success message -->
+        <?php if (isset($_GET['success'])) echo "<p class='success'>" . htmlspecialchars($_GET['success']) . "</p>"; ?>
+    </div>
+    <br>
 </body>
 </html>
