d5cf1014832a9c094a1249f7b4c3fa73a65a6536
bmorri16 <bmorri16@CompSciICv-10.fsc.int>
2025-04-30 00:51:44 -0400

    please commit to the branch

--- userP.php
+++ userP.php
@@ -16,7 +16,27 @@ if (!isset($_SESSION['usertype'])) {
     header("Location: index.php");
     exit;
 }
+
 ?>
 
-
-
+<html>
+  <head>
+    <title>Fancy Football</title>
+  <br>
+  <?php require 'dbConnect.php' ?>
+</head>
+<div class="w3-panel w3-leftbar w3-light-grey">
+    <div class="logo">
+        <h1 class="w3-cursive freshman-font">Hello Coach</h1>
+        <a href="profile.php" button type="submit" class="btn btn-sm w3-blue w3-round w3-cursive freshman-font">View Profile</button></a>
+        <a href="selectRoster.php" button type="submit" class="btn btn-sm w3-blue w3-round w3-cursive freshman-font">Test Draft</button></a>
+</div>
+</div>
+    </div>
+    <div class="prime">
+        <img src="https://npr.brightspotcdn.com/dims4/default/dcc9640/2147483647/strip/true/crop/1252x1252+0+0/resize/1760x1760!/format/webp/quality/90/?url=http%3A%2F%2Fnpr-brightspot.s3.amazonaws.com%2Flegacy%2Fsites%2Fwamc%2Ffiles%2F201504%2Fnfldraft.jpeg" alt="nfl fantasy" height="800px;">
+    </div>
+  <br>
+  <?php include 'footer.php' ?>
+</body>
+</html>
