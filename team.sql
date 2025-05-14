94382836d9a4e24e528bbc3cfcbcfe64c3701671
Braeden-moe <bmorri16@student.fitchburgstate.edu>
2025-05-13 20:32:52 -0400

    Update team.sql

--- team.sql
+++ team.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 23, 2025 at 08:19 PM
+-- Generation Time: May 14, 2025 at 02:06 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `fancyfootball`
+-- Database: `fancy football`
 --
 
 -- --------------------------------------------------------
@@ -30,9 +30,32 @@ SET time_zone = "+00:00";
 CREATE TABLE `team` (
   `TeamID` int(3) NOT NULL,
   `teamname` varchar(40) NOT NULL,
-  `dateupdate` int(10) NOT NULL,
-  `userID` int(3) NOT NULL
+  `scores` int(5) DEFAULT NULL,
+  `userID` int(3) NOT NULL,
+  `QB` varchar(50) DEFAULT NULL,
+  `RB1` varchar(50) DEFAULT NULL,
+  `RB2` varchar(50) DEFAULT NULL,
+  `WR1` varchar(50) DEFAULT NULL,
+  `WR2` varchar(50) DEFAULT NULL,
+  `TE` varchar(50) DEFAULT NULL,
+  `FLEX` varchar(50) DEFAULT NULL,
+  `D/ST` varchar(50) DEFAULT NULL,
+  `K` varchar(50) DEFAULT NULL,
+  `BE1` varchar(50) DEFAULT NULL,
+  `BE2` varchar(50) DEFAULT NULL,
+  `BE3` varchar(50) DEFAULT NULL,
+  `BE4` varchar(50) DEFAULT NULL,
+  `BE5` varchar(50) DEFAULT NULL,
+  `BE6` varchar(50) DEFAULT NULL,
+  `BE7` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `team`
+--
+
+INSERT INTO `team` (`TeamID`, `teamname`, `scores`, `userID`, `QB`, `RB1`, `RB2`, `WR1`, `WR2`, `TE`, `FLEX`, `D/ST`, `K`, `BE1`, `BE2`, `BE3`, `BE4`, `BE5`, `BE6`, `BE7`) VALUES
+(7, 'Sassy Salamanders', 0, 9, 'Brock Purdy', 'Jamyr Gibbs', 'Bijan Robinson', 'Amon-Ra St. Brown', 'Ja\'Marr Chase', 'Brock Bowers', 'Justin Jefferson', 'Broncos D/ST', 'Chris Boswell', 'Lamar Jackson', 'Brian Thomas Jr.', 'Drake London', 'Jonnu Smith', 'De\'Von Achane', 'Packers D/ST', 'Brandon Aubrey');
 
 --
 -- Indexes for dumped tables
@@ -43,6 +66,7 @@ CREATE TABLE `team` (
 --
 ALTER TABLE `team`
   ADD PRIMARY KEY (`TeamID`),
+  ADD UNIQUE KEY `userID_2` (`userID`),
   ADD KEY `userID` (`userID`);
 
 --
@@ -53,7 +77,7 @@ ALTER TABLE `team`
 -- AUTO_INCREMENT for table `team`
 --
 ALTER TABLE `team`
-  MODIFY `TeamID` int(3) NOT NULL AUTO_INCREMENT;
+  MODIFY `TeamID` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
